0be3421a529e40474af05a410f9a36ac51642ea2
nowgnas <marshmello.sw@gmail.com>
2022-12-30 02:20:05 +0900

    영속성 컨택스트2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,25 +17,54 @@ public class JpaMain {
         try {
             // 실제 동작코드 작성
 
-            // insert
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("helloB");
+            // 변경 감지
+            Member member = em.find(Member.class, 101L);
+            member.setName("zzz");
+            // 변경 시 persist를 사용하면 안됨
+
+
+            // 쓰기 지연
+//            Member member = new Member(150L, "a");
+//            Member member1 = new Member(160L, "b");
+//
+//            em.persist(member1);
 //            em.persist(member);
+//            System.out.println("persist");
+
+            // -------------------------------------------
+            // insert
+            // 비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("helloC");
+//
+//            // 영속 상태: em에 의해 관리가 된다
+//            em.persist(member); // db에 저장되지 않는 상태
+//            // 1차 캐시에 저장된다
+//
+//            Member findMember = em.find(Member.class, 101L);
+//            System.out.println("find member " + findMember.getName());
+            // -------------------------------------------
+
+            // find 1차 캐시
+//            Member member = em.find(Member.class, 1L);
+//            Member member1 = em.find(Member.class, 1L);
+//
+//            System.out.println(member1 == member);
 
             // select
 //            Member findMember = em.find(Member.class, 1L);
 //            System.out.println("find " + findMember.getName());
 
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1) // offset
-                    .setMaxResults(5) // limit
-                    .getResultList();
-            for (Member m : resultList
-            ) {
-                System.out.println("member.name " + m.getName());
-
-            }
+//            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1) // offset
+//                    .setMaxResults(5) // limit
+//                    .getResultList();
+//            for (Member m : resultList
+//            ) {
+//                System.out.println("member.name " + m.getName());
+//
+//            }
 
             // delete
 //            em.remove(findMember); // 찾은 값을 제거
