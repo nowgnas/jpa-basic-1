092051de66e6d9d89e014682ec3f32de713ee6a2
nowgnas <marshmello.sw@gmail.com>
2023-01-04 19:29:01 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,36 +1,22 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
-
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name") // db에는 name으로 column 이름이 들어간다
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING) // Enum 타입을 사용하고 싶을 때
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP) // DATE, TIME, TIMESTAMP 3개가 있다
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob // varchar를 넘어서는 큰 컨텐츠를 넣고 싶을 때
+    private String description;
 
-    public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
