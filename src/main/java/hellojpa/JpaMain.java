7017bc4b5f4beff0e280e75d07e4bf0e1fa992e0
nowgnas <marshmello.sw@gmail.com>
2023-01-05 00:46:33 +0900

    기본키 직접 할당

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,80 +15,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); // 트랜잭션 시작
         try {
-            // 실제 동작코드 작성
-
-            // 준영속
-            Member member = em.find(Member.class, 101L);
-
-            em.detach(member); // jpa에서 관리하지 않음 영속성 컨텍스트에서 관리하지 않음
-            em.clear(); // 영속성 컨텍스트 초기화 1차 캐시 모두 사라짐
-//            em.close(); // 영속성 컨텍스트 종료 트랜잭션 끝난 후 닫아줌
-
-
-
-            // 영속
-//            Member member = new Member(200L, "member");
-//            em.persist(member);
-//
-//            em.flush(); // 강제 호출: 즉시 sql 쿼리가 실행된다
-//
-//            System.out.println("-----------");
-
-            // 변경 감지
-//            Member member = em.find(Member.class, 101L);
-//            member.setName("zzz");
-            // 변경 시 persist를 사용하면 안됨
-
-
-            // 쓰기 지연
-//            Member member = new Member(150L, "a");
-//            Member member1 = new Member(160L, "b");
-//
-//            em.persist(member1);
-//            em.persist(member);
-//            System.out.println("persist");
-
-            // -------------------------------------------
-            // insert
-            // 비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("helloC");
-//
-//            // 영속 상태: em에 의해 관리가 된다
-//            em.persist(member); // db에 저장되지 않는 상태
-//            // 1차 캐시에 저장된다
-//
-//            Member findMember = em.find(Member.class, 101L);
-//            System.out.println("find member " + findMember.getName());
-            // -------------------------------------------
-
-            // find 1차 캐시
-//            Member member = em.find(Member.class, 1L);
-//            Member member1 = em.find(Member.class, 1L);
-//
-//            System.out.println(member1 == member);
-
-            // select
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("find " + findMember.getName());
-
-//            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(1) // offset
-//                    .setMaxResults(5) // limit
-//                    .getResultList();
-//            for (Member m : resultList
-//            ) {
-//                System.out.println("member.name " + m.getName());
-//
-//            }
-
-            // delete
-//            em.remove(findMember); // 찾은 값을 제거
-
-            // update
-//            findMember.setName("hellojpa");
-
+            Member member = new Member();
+            member.setId("ID");
+            member.setUsername("C");
+            em.persist(member);
             tx.commit(); // 트랜잭션 커밋
 
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,17 +6,26 @@ import java.util.Date;
 @Entity
 public class Member {
     @Id
-    private Long id;
-    @Column(name = "name") // db에는 name으로 column 이름이 들어간다
+    private String id;
+    @Column(name = "name", nullable = false) // db에는 name으로 column 이름이 들어간다
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING) // Enum 타입을 사용하고 싶을 때
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP) // DATE, TIME, TIMESTAMP 3개가 있다
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob // varchar를 넘어서는 큰 컨텐츠를 넣고 싶을 때
-    private String description;
 
+    public Member() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
