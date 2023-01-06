64b5734976440cc3d6e335aa54ebc8c6fd9f8fb1
nowgnas <marshmello.sw@gmail.com>
2023-01-07 00:26:04 +0900

    일대일

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.concurrent.locks.Lock;
 
 @Entity
 public class Member {
@@ -13,9 +14,13 @@ public class Member {
 
     //    @Column(name = "TEAM_ID")
     //    private Long teamId;
-//    @ManyToOne
-//    @JoinColumn(name = "TEAM_ID")
-//    private Team team;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Long getId() {
         return id;
